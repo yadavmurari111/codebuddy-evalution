cfbdd94a65fb251074b6a43af671a9f3497bd8ac
Murari Yadav <murari.yadav@thingmzing.com>
2023-09-21 13:48:51 +0530

    Apple login

--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -1,9 +1,18 @@
 import React, {useEffect} from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {presetBase} from './utils/color';
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
 import {webClientId} from './utils/utils';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import appleAuth from '@invertase/react-native-apple-authentication';
+import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 const logoUri = 'https://freepngimg.com/thumb/chat/1-2-chat-png-image.png';
 
@@ -47,6 +56,29 @@ const LoginScreen = ({}: any) => {
       });
   };
 
+  async function onAppleButtonPress() {
+    // Start the sign-in request
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+    });
+
+    // Ensure Apple returned a user identityToken
+    if (!appleAuthRequestResponse.identityToken) {
+      throw new Error('Apple Sign-In failed - no identify token returned');
+    }
+
+    // Create a Firebase credential from the response
+    const {identityToken, nonce} = appleAuthRequestResponse;
+    const appleCredential = await auth.AppleAuthProvider.credential(
+      identityToken,
+      nonce,
+    );
+
+    // Sign the user in with the credential
+    return auth().signInWithCredential(appleCredential);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to ChatApp</Text>
@@ -60,6 +92,18 @@ const LoginScreen = ({}: any) => {
         style={styles.anonymousSignInButton}>
         <Text style={styles.signInButtonText}>Anonymous Login</Text>
       </TouchableOpacity>
+      {Platform.OS === 'ios' && (
+        <TouchableOpacity onPress={onAppleButtonPress}>
+          <View style={styles.appleBox}>
+            <AntDesignIcons
+              name="apple1"
+              color={presetBase.colors.white}
+              size={20}
+            />
+            <Text style={styles.signInButtonText}>Sign in with Apple</Text>
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
@@ -94,12 +138,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingHorizontal: 40,
     paddingVertical: 10,
+    borderRadius: 4,
   },
   anonymousSignInButton: {
     marginTop: 10,
     backgroundColor: presetBase.colors.grey80,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 4,
+  },
+  appleBox: {
+    marginTop: 10,
+    backgroundColor: presetBase.colors.grey80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderRadius: 4,
     paddingHorizontal: 40,
     paddingVertical: 10,
   },
